506ede285294c2a2f7958b84b1321a6e26046d64
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-06-01 15:33:07 -0300

    higienize submission pile header

--- src/Renderer/Vk/SubmissionPile.cpp
+++ src/Renderer/Vk/SubmissionPile.cpp
@@ -1,5 +1,76 @@
 #include "SubmissionPile.hpp"
 
+#include <array>
+
+constexpr u64 MAX_SUBMITS = 32;
+constexpr u64 MAX_COMMAND_BUFFERS = 64;
+constexpr u64 MAX_SEMAPHORES = 64;
+
+struct SubmissionPile {
+    std::array<VkSubmitInfo2, MAX_SUBMITS> Submits;
+    std::array<VkCommandBufferSubmitInfo, MAX_COMMAND_BUFFERS> CommandBuffers;
+    std::array<VkSemaphoreSubmitInfo, MAX_SEMAPHORES> WaitSemaphores;
+    std::array<VkSemaphoreSubmitInfo, MAX_SEMAPHORES> SignalSemaphores;
+
+    u64 SubmitCount = 0;
+    u64 CmdCount = 0;
+    u64 WaitCount = 0;
+    u64 SignalCount = 0;
+
+    u64 CmdStart = 0;
+    u64 WaitStart = 0;
+    u64 SignalStart = 0;
+};
+
+void Reset(SubmissionPile& pile) {
+    pile.SubmitCount = pile.CmdCount = pile.WaitCount = pile.SignalCount = 0;
+    pile.CmdStart = pile.WaitStart = pile.SignalStart = 0;
+}
+
+SubmissionPile& Begin(SubmissionPile& pile) {
+    pile.CmdStart = pile.CmdCount;
+    pile.WaitStart = pile.WaitCount;
+    pile.SignalStart = pile.SignalCount;
+    return pile;
+}
+
+SubmissionPile& Command(SubmissionPile& pile, VkCommandBuffer command) {
+    pile.CommandBuffers[pile.CmdCount++] = {
+        VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO, nullptr,
+        command, 0
+    };
+    return pile;
+}
+
+SubmissionPile& Wait(SubmissionPile& pile, VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage) {
+    pile.WaitSemaphores[pile.WaitCount++] = {
+        VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO, nullptr,
+        semaphore, value, stage, 0
+    };
+    return pile;
+}
+
+SubmissionPile& Signal(SubmissionPile& pile, VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage) {
+    pile.SignalSemaphores[pile.SignalCount++] = {
+        VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO, nullptr,
+        semaphore, value, stage, 0
+    };
+    return pile;
+}
+
+void End(SubmissionPile& pile) {
+    u64 command_quantity = pile.CmdCount - pile.CmdStart;
+    u64 wait_semaphores_quantity = pile.WaitCount - pile.WaitStart;
+    u64 signal_semaphores_quantity = pile.SignalCount - pile.SignalStart;
+
+    pile.Submits[pile.SubmitCount++] = {
+        VK_STRUCTURE_TYPE_SUBMIT_INFO_2, nullptr, 0,
+        static_cast<u32>(wait_semaphores_quantity), wait_semaphores_quantity > 0 ? &pile.WaitSemaphores[pile.WaitStart] : nullptr,
+        static_cast<u32>(command_quantity), command_quantity > 0 ? &pile.CommandBuffers[pile.CmdStart] : nullptr,
+        static_cast<u32>(signal_semaphores_quantity), signal_semaphores_quantity > 0 ? &pile.SignalSemaphores[pile.SignalStart] : nullptr
+    };
+}
+
 void SubmitHarvestedPiles(QueueContext& ctx, VkFence execution_fence, SubmissionPile* piles, u64 pile_count) {
     std::array<VkSubmitInfo2, 128> global_submits;
     u64 total_submits = 0;
@@ -11,7 +82,7 @@ void SubmitHarvestedPiles(QueueContext& ctx, VkFence execution_fence, Submission
             global_submits[total_submits++] = pile.Submits[j];
         }
 
-        pile.Reset();
+        Reset(pile);
     }
 
     if (total_submits > 0) {
--- src/Renderer/Vk/SubmissionPile.hpp
+++ src/Renderer/Vk/SubmissionPile.hpp
@@ -1,76 +1,19 @@
 #pragma once
 
-#include <array>
-
 #include "Renderer/VkVault.hpp"
 
-constexpr u64 MAX_SUBMITS = 32;
-constexpr u64 MAX_COMMAND_BUFFERS = 64;
-constexpr u64 MAX_SEMAPHORES = 64;
+struct SubmissionPile;
 
-struct SubmissionPile {
-    std::array<VkSubmitInfo2, MAX_SUBMITS> Submits;
-    std::array<VkCommandBufferSubmitInfo, MAX_COMMAND_BUFFERS> CommandBuffers;
-    std::array<VkSemaphoreSubmitInfo, MAX_SEMAPHORES> WaitSemaphores;
-    std::array<VkSemaphoreSubmitInfo, MAX_SEMAPHORES> SignalSemaphores;
+void Reset(SubmissionPile& pile);
 
-    u64 SubmitCount = 0;
-    u64 CmdCount = 0;
-    u64 WaitCount = 0;
-    u64 SignalCount = 0;
+SubmissionPile& Begin(SubmissionPile& pile);
 
-    u64 CmdStart = 0;
-    u64 WaitStart = 0;
-    u64 SignalStart = 0;
+SubmissionPile& Command(SubmissionPile& pile, VkCommandBuffer command);
 
-    void Reset() {
-        SubmitCount = CmdCount = WaitCount = SignalCount = 0;
-        CmdStart = WaitStart = SignalStart = 0;
-    }
+SubmissionPile& Wait(SubmissionPile& pile, VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage = 0);
 
-    SubmissionPile& Begin() {
-        CmdStart = CmdCount;
-        WaitStart = WaitCount;
-        SignalStart = SignalCount;
-        return *this;
-    }
+SubmissionPile& Signal(SubmissionPile& pile, VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage = 0);
 
-    SubmissionPile& Command(VkCommandBuffer command) {
-        CommandBuffers[CmdCount++] = {
-            VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO, nullptr,
-            command, 0
-        };
-        return *this;
-    }
-
-    SubmissionPile& Wait(VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage = 0) {
-        WaitSemaphores[WaitCount++] = {
-            VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO, nullptr,
-            semaphore, value, stage, 0
-        };
-        return *this;
-    }
-
-    SubmissionPile& Signal(VkSemaphore semaphore, u64 value, VkPipelineStageFlags2 stage = 0) {
-        SignalSemaphores[SignalCount++] = {
-            VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO, nullptr,
-            semaphore, value, stage, 0
-        };
-        return *this;
-    }
-
-    void End() {
-        u64 command_quantity = CmdCount - CmdStart;
-        u64 wait_semaphores_quantity = WaitCount - WaitStart;
-        u64 signal_semaphores_quantity = SignalCount - SignalStart;
-
-        Submits[SubmitCount++] = {
-            VK_STRUCTURE_TYPE_SUBMIT_INFO_2, nullptr, 0,
-            static_cast<u32>(wait_semaphores_quantity), wait_semaphores_quantity > 0 ? &WaitSemaphores[WaitStart] : nullptr,
-            static_cast<u32>(command_quantity), command_quantity > 0 ? &CommandBuffers[CmdStart] : nullptr,
-            static_cast<u32>(signal_semaphores_quantity), signal_semaphores_quantity > 0 ? &SignalSemaphores[SignalStart] : nullptr
-        };
-    }
-};
+void End(SubmissionPile& pile);
 
 void SubmitHarvestedPiles(QueueContext& ctx, VkFence execution_fence, SubmissionPile* piles, u64 pile_count);
