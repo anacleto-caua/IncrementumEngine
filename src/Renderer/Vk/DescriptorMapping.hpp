de2fa008dc19f5b5999a682aff3b99f9e0d6b88b
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-06-17 18:29:15 -0300

    track used descriptor sets and bindings indexes

---
+++ src/Renderer/Vk/DescriptorMapping.hpp
@@ -0,0 +1,33 @@
+/**
+ * This single file is responsible for tracking the currently used drescriptor set and binding indexes
+ * thoose are static and editing them will require manual changes on the shaders to avoid conflict.
+ */
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+namespace DescriptorMap {
+    // ================================================
+    // SET 0: Global & Static Data (Rarely changes)
+    // ================================================
+    namespace Global {
+        inline constexpr u32 SetIndex = 0;
+    }
+
+    // ================================================
+    // SET 1: Per-Frame Data (Changes every frame)
+    // ================================================
+    namespace PerFrame {
+        inline constexpr u32 SetIndex = 1;
+
+        inline constexpr u32 Binding_ChunkDrawListSSBO = 0;
+        inline constexpr u32 Binding_HeightmapTexture = 1;
+    }
+
+    // ================================================
+    // SET 2: Material/Entity Data (Changes per object)
+    // ================================================
+    namespace Material {
+        inline constexpr u32 SetIndex = 2;
+    }
+}
